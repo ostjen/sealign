cea6e9fe6b2fb16971c469a66e856a74b8128bff
Leonardo <leonardoostjen@gmail.com>
2019-03-12 22:55:16 -0300

    started text treatment

--- tokenizer.py
+++ tokenizer.py
@@ -1,10 +1,9 @@
-import nltk
+﻿import nltk
 from nltk.tokenize import sent_tokenize, word_tokenize
 import numpy as np
 import sys
 import fileinput
-
-
+#nltk.download('punkt')
 
 
 
@@ -18,21 +17,11 @@ def split_sentence(input_file , output_name):
 	out_file = open(output_name,"a+")
 	
 	for element in out_list:
-		out_file.write(element + '￭')
+		out_file.write(element + '*')
 	
 	out_file.close()
 	
 	return out_list
-#________________________________________________________________________________________________________________________________________________________________
-
- #def add_space(input_file):
-#	text = open(input_file,"r+")
-#	    sys.stdout.write(' {l}'.format(l=line))
-#	    sys.stdout.write("{} \n".format(line.rstrip()))
-
-
-#_________________________________________________________________________________________________________________________________________________________________
-
 
 
 split_sentence(sys.argv[1] , sys.argv[2])
