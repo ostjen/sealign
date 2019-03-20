c43c657e334673793de376b5728655dacb213395
Leonardo <leonardoostjen@gmail.com>
2019-03-20 10:53:42 -0300

    solved matrix generation bug

--- embedding_related.py
+++ embedding_related.py
@@ -123,5 +123,5 @@ def generate_matrix(src_model, tgt_model, src_sentences, tgt_sentences):
         for j in range(0, len(tgt_sentences)):
             distances.append(wmdistance_b(src_model, tgt_model, src_sentences[i], tgt_sentences[j]))
         matrix = np.insert(matrix, i, distances, 0)
-    from datetime import datetime
-    np.savetxt("./results/result_matrix-" + datetime.now().strftime('%Y-%m-%d %H:%M' + '.csv'), matrix, delimiter=",")
+    #from datetime import datetime
+    np.savetxt("./results/result_matrix.csv", matrix, delimiter=",")
--- sealign.py
+++ sealign.py
@@ -9,16 +9,16 @@ nmax = 150000
 print('\ncarregando modelos ...')
 src_model = gensim.models.KeyedVectors.load_word2vec_format(src_path, limit=nmax)
 tgt_model = gensim.models.KeyedVectors.load_word2vec_format(tgt_path, limit=nmax)
-
+"""
 print('\nsource language')
 src_l = input("Input ")
 print('\ntarget language')
 tgt_l = input("Input ")
+"""
+src_sentences = load_care(sys.argv[1],'english')
+tgt_sentences = load_care(sys.argv[2],'german')
 
-src_sentences = load_care(sys.argv[1],src_l)
-tgt_sentences = load_care(sys.argv[2],tgt_l)
-
-generate_matrix (src_model, tgt_model, src_sentences, tgt_sentences)
+generate_matrix (src_model, tgt_model, src_sentences[:50], tgt_sentences[:50])
 
 
 
