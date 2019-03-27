d3a9da32beab37ad7f7b7e8be29bcd8844270b1b
Leonardo <leonardoostjen@gmail.com>
2019-03-26 23:16:50 -0300

    organized repo

--- sealign.py
+++ sealign.py
@@ -5,23 +5,27 @@ import sys
 
 src_path = '~/desktop/embeddings/wiki.en.align.vec'
 tgt_path = '~/desktop/embeddings/wiki.es.align.vec'
-
 en_in = open('./samples/test-enes.en', encoding='utf8').readlines()
 es_in = open('./samples/test-enes.es', encoding='utf8').readlines()
 
 #raw input will be used later
 english_text = load_care(en_in,'english')
 spanish_text = load_care(es_in,'spanish')
-
+#---------------loading word2vec models ------------------------------------------------------------------------
 print('\nloading models ...')
 src_model = gensim.models.KeyedVectors.load_word2vec_format(src_path)
 print('\nsrc model loaded')
 tgt_model = gensim.models.KeyedVectors.load_word2vec_format(tgt_path)
 print('\ndone')
+#---------------------------------------------------------------------------------------------------------------
 
 matrix = generate_matrix(src_model, tgt_model, english_text[:50], spanish_text[:50],csv = 'yes')
+
+#list with the min value index of each column
 spanish_index = np.argmin(matrix,axis = 0)
-out = open('aligned_out2.txt',"w+",encoding = 'utf-8')
+
+#generate bilingual aligned sentences in a txt file
+out = open('aligned_out.txt',"w+",encoding = 'utf-8')
 for ind in range(0,len(en_in[:50])):
     aux = en_in[ind].replace('\n','\t')
     print(aux + es_in[spanish_index[ind]])
